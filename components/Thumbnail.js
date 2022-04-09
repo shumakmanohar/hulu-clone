99bc2246b74524a7cc3ebc6eb9b7a118d433dd50
shumakmanohar <shumakmanohar123@gmail.com>
2022-04-09 12:08:24 +0530

    Updated the Thumbnail.js

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,28 +1,26 @@
-import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
+import Image from "next/image";
 import { forwardRef } from "react";
 
 const Thumbnail = forwardRef(({ result }, ref) => {
-	const BaseURL = "https://image.tmdb.org/t/p/original";
-	//Line that matters!!!
-	Thumbnail.displayName = "Thumbnail";
+	const BASE_URL = "https://image.tmdb.org/t/p/original/";
+
 	return (
 		<div
 			ref={ref}
-			className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+			className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-x-105 hover:z-50"
 		>
 			<Image
 				layout="responsive"
 				src={
-					`
-            ${BaseURL}${result.backdrop_path || result.poster_path}` ||
-					`${BaseURL}${result.poster_path}`
+					`${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+					`${BASE_URL}${result.poster_path}`
 				}
 				height={1080}
 				width={1920}
 			/>
 			<div className="p-2">
-				<p className="truncate">{result.overview}</p>
+				<p className="truncate max-w-md">{result.overview}</p>
 				<h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
 					{result.title || result.original_name}
 				</h2>
